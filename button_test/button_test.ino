39f7f42e9d22cb8dd7f749aeb98188b7e2bda88b
whoami90506 <whoami90506@github.com>
2019-06-15 01:37:06 +0800

    add 7688 test button

---
+++ button_test/button_test.ino
@@ -0,0 +1,13 @@
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(12);
+  digitalWrite(13, val);
+}
