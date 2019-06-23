d0a64608605657a82689367ce68bf38086550638
team06_dclab <team06@dclab.com>
2019-06-23 14:29:42 +0800

    fix fucking error

--- src/DisplayROM/Tank0Display.sv
+++ src/DisplayROM/Tank0Display.sv
@@ -14,7 +14,8 @@ module Tank0Display(
 logic [ 1:0] mem [0:2499];
 logic [23:0] center [0:3];
 
-logic [ 5:0] x_w, y_w;
+logic [ 5:0] x_w;
+logic [ 5:0] y_w;
 
 assign x_w = (i_game_x + 6'd2 - i_tank_x) * 4'd10 + i_grid_x;
 assign y_w = (i_game_y + 6'd2 - i_tank_y) * 4'd10 + i_grid_y;
@@ -24,10 +25,10 @@ always_comb begin
 		i_game_y >= i_tank_y - 6'd2 && i_game_y <= i_tank_y + 6'd2) begin
 
 		case (i_tank_dir)
-			2'd0 : o_rgb_w = center[mem[         y_w  * 6'd50 +          x_w ]]; // up
-			2'd1 : o_rgb_w = center[mem[(6'd49 - y_w) * 6'd50 + (6'd49 - x_w)]]; // down
-			2'd2 : o_rgb_w = center[mem[         x_w  * 6'd50 + (6'd49 - y_w)]]; // left
-			2'd3 : o_rgb_w = center[mem[(6'd49 - x_w) * 6'd50 +          y_w ]]; // right
+			2'd0 : o_rgb_w = center[mem[      (y_w  * 50) +          x_w ]]; // up
+			2'd1 : o_rgb_w = center[mem[((49 - y_w) * 50) + (49 - x_w)]]; // down
+			2'd2 : o_rgb_w = center[mem[      (x_w  * 50) + (49 - y_w)]]; // left
+			2'd3 : o_rgb_w = center[mem[((49 - x_w) * 50) +          y_w ]]; // right
 		endcase
 	end else o_rgb_w = 24'h0;
 end
