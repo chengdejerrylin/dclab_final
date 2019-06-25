5d7f7c4bde9f1802a83596a03c9e74b13fdcf3e7
whoami90506 <whoami90506@github.com>
2019-06-25 17:21:50 +0800

    add sand background

--- resource/imgResize.py
+++ resource/imgResize.py
@@ -8,5 +8,5 @@ if float(im.size[1]) / im.size[0] == 9.0/16.0 :
 	im = im.crop((w*2.0/16.0, 0, w*14.0/16.0, h))
 	im.save(sys.argv[1][:-4] + '_crop.jpg')
 
-im = im.resize((320,240), Image.NEAREST)
-im.save(sys.argv[1][:-4] + '_320x240.bmp')
+im = im.resize((320,220), Image.NEAREST)
+im.save(sys.argv[1][:-4] + '_320x220.bmp')
